3af7599469a32f93cda1435355e115f24affe3da
Shad0w <ioni.xyz@gmail.com>
2024-04-19 11:36:54 +0300

    Create tmux.sh

---
+++ tmux.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir ~/.config/tmux -p
+cd ~/.config/tmux && wget https://raw.githubusercontent.com/Shad9w/config/main/tmux.conf
+tmux
