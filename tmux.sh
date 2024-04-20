662235a642e64f26dab1ade8c644c1cf07c0f61e
Shad0w <ioni.xyz@gmail.com>
2024-04-20 13:09:25 +0300

    old config rename

--- tmux.sh
+++ tmux.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mkdir ~/.config/tmux -p
+mv ~/.config/tmux/tmux.conf ~/.config/tmux/oldconfig.txt
 cd ~/.config/tmux && wget https://raw.githubusercontent.com/Shad9w/config/main/tmux.conf
 tmux
