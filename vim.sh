bb52cd7e4ac1ade19db2acb9ec0312e8e2a21746
Shad0w <ioni.xyz@gmail.com>
2024-04-23 15:30:35 +0300

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -7,9 +7,12 @@ echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
 echo 'run :MasonInstallAll'
 
 sudo apt install gcc make
-git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+git clone https://github.com/NvChad/starter ~/.config/nvim
 
 echo 'alias vi=nvim' >> ~/.bashrc
 echo 'alias vim=nvim' >> ~/.bashrc
 
 source ~/.bashrc
+
+echo 'run :MasonInstallAll'
+nvim
