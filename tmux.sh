ef210a577e3c962a6061f391bf01df35a6ebd801
Shad0w <ioni.xyz@gmail.com>
2024-04-23 14:37:15 +0300

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -2,4 +2,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mkdir ~/.config/tmux -p
 mv ~/.config/tmux/tmux.conf ~/.config/tmux/oldconfig.txt
 cd ~/.config/tmux && wget https://raw.githubusercontent.com/Shad9w/config/main/tmux.conf
+rm ~/.tmux.conf
+cd ~ && wget https://raw.githubusercontent.com/Shad9w/config/main/.tmux.conf
 tmux
