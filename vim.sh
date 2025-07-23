1dfb1e99407318e77fac4174f66314dc68bb7aa8
Shad0w <ioni.xyz@gmail.com>
2025-07-23 10:46:02 +0300

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,8 +1,8 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
 
 echo 'run :MasonInstallAll'
 
